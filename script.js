f2771f38dc24f9e85e2a414559885e6f53ce9b93
Benco351 <83662617+Benco351@users.noreply.github.com>
2021-09-02 18:28:50 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 if ('serviceWorker' in navigator) {
 
   navigator.serviceWorker
-    .register('service-worker.js')
+     .register('./service-worker.js', { scope: './' })
     .then(function(registration) {
       console.log("Service Worker Registered:", registration.scope);
-    });
+    })
     .catch(function(err) {
       console.log("Service Worker Failed to Register", err);
-    });
+    })
 }
 
 let theme = localStorage.getItem('theme')
