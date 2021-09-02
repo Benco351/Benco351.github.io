f14dea5d93f6179149913a95a20354423bd33c69
Benco351 <83662617+Benco351@users.noreply.github.com>
2021-09-02 18:13:42 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 if ('serviceWorker' in navigator) {
 
   navigator.serviceWorker
-    .register('/service-worker.js', { scope: '/' })
+    .register('service-worker.js')
     .then(function(registration) {
-      console.log("Service Worker Registered");
+      console.log("Service Worker Registered", registration.scope);
     })
     .catch(function(err) {
       console.log("Service Worker Failed to Register", err);
